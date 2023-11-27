807278deca02717c98a864a02c9059266d1b60c2
Michael Berinshteyn <berinsht001@gannon.edu>
2023-11-27 04:45:32 +0000

    Add validation attributes to request models

--- backend/src/UserApi/Models/AuthenticateRequest.cs
+++ backend/src/UserApi/Models/AuthenticateRequest.cs
@@ -1,21 +1,25 @@
-namespace UserApi.Models;
-
-using System.ComponentModel.DataAnnotations;
-
-/// <summary>
-/// Represents a request to authenticate a user.
-/// </summary>
-public class AuthenticateRequest
+// AuthenticateRequest.cs
+namespace UserApi.Models
 {
-    /// <summary>
-    /// The username of the user to authenticate.
-    /// </summary>
-    [Required]
-    public string Username { get; set; } = null!;
+    using System.ComponentModel.DataAnnotations;
 
     /// <summary>
-    /// The password of the user to authenticate.
+    /// Represents a request to authenticate a user.
     /// </summary>
-    [Required]
-    public string Password { get; set; } = null!;
+    public class AuthenticateRequest
+    {
+        /// <summary>
+        /// The username of the user to authenticate.
+        /// </summary>
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
+        public string Username { get; set; } = null!;
+
+        /// <summary>
+        /// The password of the user to authenticate.
+        /// </summary>
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters.")]
+        public string Password { get; set; } = null!;
+    }
 }
--- backend/src/UserApi/Models/CreateUserRequest.cs
+++ backend/src/UserApi/Models/CreateUserRequest.cs
@@ -1,33 +1,34 @@
+// CreateUserRequest.cs
 using System.ComponentModel.DataAnnotations;
 
-namespace UserApi.Models;
-
-/// <summary>
-/// Represents a request to create a new user.
-/// </summary>
-
-public class CreateUserRequest
+namespace UserApi.Models
 {
-    [Required]
-    [StringLength(50)]
-    public string FirstName { get; set; } = null!;
+    /// <summary>
+    /// Represents a request to create a new user.
+    /// </summary>
+    public class CreateUserRequest
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; } = null!;
 
-    [Required]
-    [StringLength(50)]
-    public string LastName { get; set; } = null!;
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; } = null!;
 
-    [Required]
-    [StringLength(50)]
-    public string Username { get; set; } = null!;
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
+        public string Username { get; set; } = null!;
 
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
 
-    [Required]
-    [MinLength(8)]
-    public string Password { get; set; } = null!;
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters.")]
+        public string Password { get; set; } = null!;
 
-    [RoleValidation]
-    public string Role { get; set; } = "Member";
+        [RoleValidation]
+        public string Role { get; set; } = "Member";
+    }
 }
